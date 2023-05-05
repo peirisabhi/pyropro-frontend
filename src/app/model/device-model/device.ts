7acebb8c4ffa6977fade416ba0081b96a605bd36
abhishek peiris <abhishekdananjaya1@yahoo.com>
2023-05-05 23:36:18 +0530

    fire detection component and fire prediction component added

---
+++ src/app/model/device-model/device.ts
@@ -0,0 +1,8 @@
+export class Device {
+  id?: number;
+  device_id?: string;
+  lat?: string;
+  lan?: string;
+  district?: string;
+  user?: string;
+}
---
+++ src/app/model/fire-model/fire.ts
@@ -0,0 +1,11 @@
+export class Fire {
+  id?: number;
+  temperature?: number;
+  humidity?: number;
+  time?: string;
+  device_id?: string;
+  wind_speed?: number;
+  rain?: number;
+  flame_detection?: boolean;
+
+}
